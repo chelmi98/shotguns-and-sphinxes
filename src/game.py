400b0f465d1c4ab8c6eeba25538ca2af90b6fb83
chelmi98 <chelmi98@gmail.com>
2013-08-17 19:53:37 -0600

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,12 +1,56 @@
+import random
+
 running=True
 
+class mob(object):
+    def __init__(self,race,name,gender,maxhealth,baseattack):
+        self.race=race
+        self.name=name
+        self.gender=gender
+        self.maxhealth=maxhealth
+        self.health=maxhealth
+        self.baseattack=baseattack
+        self.dead=False
+        self.seen=False
+
+    def attack(self,target):
+        if target.dead==False:
+            if random.randint(1,2)==2:
+                target.health-=self.baseattack
+                print('You hit the '+target.race+'.')
+            else:
+                print('You miss the '+target.race+'.')
+        else:
+            print('The '+target.race+' is already dead.')
+
+    def update(self):
+        if self.health<0 and self.dead==False:
+            self.health=0
+            self.dead=True
+            print('The '+self.race+' is dead.')
+
+    def describe(self):
+        if self.seen==False:
+            print('You see a '+self.race)
+            self.seen=True
+
+you=mob('human','player','m',100,20)
+
+mobs=[]
+mobs+=[mob('beetle','none','m',50,10)]
+
 def describe():
-    print('You see a cat.')
+    for i in mobs:
+        i.describe()
 
 def tick(response):
     response=response.lower()
     response=response.split(' ')
-    print response
+    if response[0]=='attack':
+        you.attack(mobs[0])
+        mobs[0].update()
+    else:
+        print(response[0]+' isnt a command.')
 
 def confirm():
     print('Are you sure?')
