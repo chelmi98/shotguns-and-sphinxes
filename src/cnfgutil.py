ee911f1b526d4d84c0e022fad3a541e22d26c5b8
chelmi98 <chelmi98@gmail.com>
2013-08-21 14:11:05 -0600

    More work on items

--- src/cnfgutil.py
+++ src/cnfgutil.py
@@ -7,11 +7,21 @@ def readFile(path,name):
     tmp=open(name)
     tmp2=tmp.readlines()
     tmp.close()
+
+    for i in range(len(tmp2)):
+        tmp2[i]=tmp2[i].strip()
+
+    return tmp2
+
+def reaCnfg(path,name):
+    os.chdir(home+path)
+    tmp=open(name)
+    tmp2=tmp.readlines()
+    tmp.close()
     tmp3={}
 
     for i in range(len(tmp2)):
         tmp2[i]=tmp2[i].strip()
-        print(tmp2[i])
         tmp3[tmp2[i][:tmp2[i].index('=')]]=tmp2[i][tmp2[i].index('=')+1:]
 
     return tmp3
