f9b1f284079dbf82defa9ab39788e0ccf3927c7d
chelmi98 <chelmi98@gmail.com>
2013-09-15 15:55:08 -0600

    Moving to more modular format

--- src/cnfgutil.py
+++ src/cnfgutil.py
@@ -2,18 +2,22 @@ import os
 
 home=os.getcwd()
 
-def readFile(path,name):
+def readFile(path, name, strip=True):
     os.chdir(home+path)
-    tmp=open(name)
-    tmp2=tmp.readlines()
+    tmp = open(name)
+    tmp2 = tmp.readlines()
     tmp.close()
 
-    for i in range(len(tmp2)):
-        tmp2[i]=tmp2[i].strip()
+    if strip:
+        for i in range(len(tmp2)):
+            tmp2[i]=tmp2[i].strip()
+    else:
+        for i in range(len(tmp2)):
+            tmp2[i]=tmp2[i].rstrip()
 
     return tmp2
 
-def reaCnfg(path,name):
+def readCnfg(path,name):
     os.chdir(home+path)
     tmp=open(name)
     tmp2=tmp.readlines()
--- src/game.py
+++ src/game.py
@@ -1,58 +1,45 @@
 import random
 import cnfgutil
 
-running=True
+title=cnfgutil.readFile('//assets','title.txt',False)
+for i in title:
+    print(i)
+print('')
+
+running = True
 
 class mob(object):
-    def __init__(self,race,name,gender,maxhealth,baseattack):
-        self.race=race
-        self.name=name
-        if name=='none':
-            self.title='the '+race
-        else:
-            self.title=name
-        self.gender=gender
-        self.maxhealth=maxhealth
-        self.health=maxhealth
-        self.baseattack=baseattack
-        self.dead=False
-        self.seen=False
-
-    def attack(self,target):
-        if target.dead==False:
-            if random.randint(1,3)==3:
-                print('You miss '+target.title+'.')
-            else:
-                target.health-=self.baseattack
-                print('You hit '+target.title+'.')
-        else:
-            print(target.title+' is already dead.')
-
-    def update(self):
-        if self.health<0 and self.dead==False:
-            self.health=0
-            self.dead=True
-            print(self.title+' is dead.')
-
-    def describe(self):
-        if self.seen==False:
-            print('You see '+self.title+'.')
-            self.seen=True
-
-class item(object):
     def __init__(self,path,name):
-        stats=cnfgutil.reaCnfg(path,name)
+        stats=cnfgutil.readCnfg(path,name)
+
+        self.desc=[]; c=0
+        while True:
+            try:
+                self.desc+=[stats['desc'+str(c)]]
+                c+=1
+            except:
+                break
+
+        self.name = stats['name']
+        self.gender = stats['gender']
+        self.mobtype = stats['type']
+        self.health = stats['health']
+        self.atkbase = stats['baseatk']
+        self.atktype = stats['atktype']
+        self.atkrange = stats['range']
+        self.hitdesc = stats['hitdesc0']
+        self.missdesc = stats['missdesc0']
+
+bob=mob('//assets//mobs','mummie.txt')
+
+class wep(object):
+    def __init__(self,path,name):
+        stats=cnfgutil.readCnfg(path,name)
         self.dmg=stats['damage']
         print(self.dmg)
 
-you=mob('human','player','m',100,20)
-
-mobs=[]
-mobs+=[mob('beetle','none','m',50,10)]
-
 def describe():
-    for i in mobs:
-        i.describe()
+    pass
 
 def tick(response):
     global running
@@ -62,9 +49,6 @@ def tick(response):
     if response[0]=='quit':
         if confirm():
             running=False
-    elif response[0]=='attack':
-        you.attack(mobs[0])
-        mobs[0].update()
     else:
         print(response[0]+' isnt a command.')
 
