f7c6f560727391d742c789e282b7ba825906b22f
chelmi98 <chelmi98@gmail.com>
2013-09-15 17:13:23 -0600

    made more readable

--- src/cnfgutil.py
+++ src/cnfgutil.py
@@ -1,35 +1,34 @@
 import os
 
-home=os.getcwd()
+home = os.getcwd()
 
-def readFile(path, name, strip=True):
-    os.chdir(home+path)
+def readFile (path, name, strip = True):
+    os.chdir(home + path)
     tmp = open(name)
     tmp2 = tmp.readlines()
     tmp.close()
 
     if strip:
         for i in range(len(tmp2)):
-            tmp2[i]=tmp2[i].strip()
+            tmp2[i] = tmp2[i].strip()
     else:
         for i in range(len(tmp2)):
-            tmp2[i]=tmp2[i].rstrip()
+            tmp2[i] = tmp2[i].rstrip()
 
     return tmp2
 
-def readCnfg(path,name):
-    os.chdir(home+path)
-    tmp=open(name)
-    tmp2=tmp.readlines()
+def readCnfg (path, name):
+    os.chdir(home + path)
+    tmp = open(name)
+    tmp2 = tmp.readlines()
     tmp.close()
-    tmp3={}
+    tmp3 = {}
 
     for i in range(len(tmp2)):
-        tmp2[i]=tmp2[i].strip()
+        tmp2[i] = tmp2[i].strip()
         try:
-            tmp3[tmp2[i][:tmp2[i].index('=')]]=int(tmp2[i][tmp2[i].index('=')+1:])
+            tmp3[tmp2[i][:tmp2[i].index('=')]] = int(tmp2[i][tmp2[i].index('=') + 1:])
         except:
-            tmp3[tmp2[i][:tmp2[i].index('=')]]=tmp2[i][tmp2[i].index('=')+1:]
-
+            tmp3[tmp2[i][:tmp2[i].index('=')]] = tmp2[i][tmp2[i].index('=') + 1:]
 
     return tmp3
